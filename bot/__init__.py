71b71d45b77f3df43fa08a0bca697b42beed8eed
kimoland <85030146+kimoland@users.noreply.github.com>
2022-06-12 22:30:37 -0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -103,7 +103,7 @@ async def download(event):
                         ])
                 #else :
                     #await event.delete()
-                event.raw_text == "❓درباره ما":
+                if event.raw_text == "❓درباره ما":
                     await event.reply(f"👤درباره ما\n\n↯طراحی: KingNetwork\n↯سرور: [Exclusive](https://t.me/King_network7)\n↯ورژن: 1.0.3\n↯لینک: نیم بها\n↯حمایت: [دونیت](https://www.payping.ir/d/WiZG)\n\n🆔 @{Config.CHANNEL_USERNAME}",link_preview=False)
             
         elif event.is_channel:
